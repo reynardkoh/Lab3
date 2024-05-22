3721b4e01a9e527a637c9dd459f444546099391d
reynardkoh <reynardkoh@gmail.com>
2024-05-22 16:16:01 +0800

    Modified Lab3

--- Lab3.py
+++ Lab3.py
@@ -12,29 +12,37 @@ def bubble_sort(arr, sorting_order):
     # Get number of elements in the list
     n = len(arr_result)
 
-    if n < 10:
+    # Checking array for non integers
+    for i in arr_result:
+        if type(i) != int:
+            arr_result = 2
+                    
+    if(arr_result != 2):
+        if (0 < n < 10):
         # Traverse through all array elements
-        for i in range(n - 1):
+            for i in range(n - 1):
             # range(n) also work but outer loop will
             # repeat one time more than needed.
 
             # Last i elements are already in place
-            for j in range(0, n - i - 1):
+                for j in range(0, n - i - 1):
 
-                if sorting_order == SORT_ASCENDING:
-                    if arr_result[j] > arr_result[j + 1]:
-                        arr_result[j], arr_result[j + 1] = arr_result[j + 1], arr_result[j]
+                    if sorting_order == SORT_ASCENDING:
+                        if arr_result[j] > arr_result[j + 1]:
+                            arr_result[j], arr_result[j + 1] = arr_result[j + 1], arr_result[j]
 
 
-                elif sorting_order == SORT_DESCENDING:
-                    if arr_result[j] < arr_result[j + 1]:
-                        arr_result[j], arr_result[j + 1] = arr_result[j + 1], arr_result[j]
+                    elif sorting_order == SORT_DESCENDING:
+                        if arr_result[j] < arr_result[j + 1]:
+                            arr_result[j], arr_result[j + 1] = arr_result[j + 1], arr_result[j]
 
                 else:
                     # Return an empty array
                     arr_result = []
-    else:
-        arr_result = -1
+        elif n == 0:
+            arr_result = 0
+        else:
+            arr_result = 1
 
     return arr_result
 
