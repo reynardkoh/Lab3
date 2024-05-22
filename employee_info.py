393c810f5d9c0d9af0e9f1491812c67321c4b4cf
reynardkoh <reynardkoh@gmail.com>
2024-05-23 00:41:07 +0800

    Calculate Average Employee Salary Function

--- employee_info.py
+++ employee_info.py
@@ -21,6 +21,11 @@ def get_employees_by_age_range(age_lower_limit, age_upper_limit):
 def calculate_average_salary():
     total = 0
     average = 0
+    
+    for item in employee_data:
+        total = total + item["salary"]
+
+    average = total/len(employee_data)
 
     #add your implementation to calculate here
 
