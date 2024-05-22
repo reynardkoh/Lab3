94bec74b262a559468a218102cc40340d54d81bb
reynardkoh <reynardkoh@gmail.com>
2024-05-22 23:43:33 +0800

    price_info Unit Tests

---
+++ Test_price_info.py
@@ -0,0 +1,23 @@
+import price_info
+
+def test_total_cost_shopping():
+    price_list={'apple' : 1.20, 'orange':1.40, 'watermelon': 6.50, 'pineapple': 2.70, 'pear' : 0.90, 'papaya': 2.95, 'pomegranate': 4.95 }
+
+    quantity_list= {'apple': 5, 'orange':5, 'watermelon': 1, 'pineapple': 2, 'pear' : 10, 'papaya': 1, 'pomegranate': 2}
+
+    result = price_info.total_cost_shopping()
+
+    assert (result == 46.75)
+
+def test_cost_of_fruit():
+    price_list={'apple' : 1.20, 'orange':1.40, 'watermelon': 6.50, 'pineapple': 2.70, 'pear' : 0.90, 'papaya': 2.95, 'pomegranate': 4.95 }
+
+    quantity_list= {'apple': 5, 'orange':5, 'watermelon': 1, 'pineapple': 2, 'pear' : 10, 'papaya': 1, 'pomegranate': 2}
+
+    result = price_info.cost_of_fruits("apple",10)
+
+    assert (result == 12.0)
+
+
+
+
